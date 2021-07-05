4bbd2803974c52a440bc497179c1bb1868d227dd
Irsyadhani Dwi Shubhi <58015078+irsyadhani@users.noreply.github.com>
2021-07-05 12:42:02 +0700

    Update SQL

--- primalangga_store.sql
+++ primalangga_store.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 21, 2021 at 06:50 AM
--- Server version: 10.4.19-MariaDB
--- PHP Version: 8.0.7
+-- Generation Time: Jul 05, 2021 at 07:40 AM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -42,12 +42,57 @@ CREATE TABLE `tb_barang` (
 --
 
 INSERT INTO `tb_barang` (`tb_brg`, `nama_brg`, `keterangan`, `kategori`, `harga`, `stok`, `gambar`) VALUES
-(1, 'modem1', 'Modem Elektronik mf90', 'Modem', 100000, 50, 'mf90.png'),
+(1, 'modem1', 'Modem Elektronik mf95', 'Modem', 100000, 50, 'mf90.png'),
 (2, 'modem2', 'Modem Elektronik MFMFAN', 'Modem', 120000, 50, 'MFMFAN.png'),
 (3, 'modem3', 'Modem Elektronik 3 in 1', 'Modem', 140000, 50, '3 in 1.jpeg'),
 (4, 'modem4', 'Modem Elektronik Portable', 'Modem', 160000, 50, 'portable.jpeg'),
 (5, 'modem5', 'Modem Elektronik Receiver', 'Modem', 180000, 50, 'receiver.jpeg'),
 (9, 'Modem Aquila', 'modem 4g aquila', 'Modem', 198000, 34, 'tsel_4.png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_invoice`
+--
+
+CREATE TABLE `tb_invoice` (
+  `id_invoice` int(11) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `alamat` varchar(200) NOT NULL,
+  `tgl_pesan` datetime NOT NULL,
+  `batas_bayar` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_invoice`
+--
+
+INSERT INTO `tb_invoice` (`id_invoice`, `nama`, `alamat`, `tgl_pesan`, `batas_bayar`) VALUES
+(4, 'Irsyadhani Dwi Shubhi', 'Perintis III', '2021-07-05 11:26:07', '2021-07-06 11:26:07');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_pesanan`
+--
+
+CREATE TABLE `tb_pesanan` (
+  `id_pesanan` int(11) NOT NULL,
+  `id_invoice` int(11) NOT NULL,
+  `tb_brg` int(11) NOT NULL,
+  `nama_brg` varchar(50) NOT NULL,
+  `jumlah` int(3) NOT NULL,
+  `harga` int(10) NOT NULL,
+  `pilihan` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_pesanan`
+--
+
+INSERT INTO `tb_pesanan` (`id_pesanan`, `id_invoice`, `tb_brg`, `nama_brg`, `jumlah`, `harga`, `pilihan`) VALUES
+(3, 4, 1, 'modem1', 1, 100000, ''),
+(4, 4, 2, 'modem2', 2, 120000, '');
 
 --
 -- Indexes for dumped tables
@@ -60,6 +105,18 @@ ALTER TABLE `tb_barang`
   ADD PRIMARY KEY (`tb_brg`);
 
 --
+-- Indexes for table `tb_invoice`
+--
+ALTER TABLE `tb_invoice`
+  ADD PRIMARY KEY (`id_invoice`);
+
+--
+-- Indexes for table `tb_pesanan`
+--
+ALTER TABLE `tb_pesanan`
+  ADD PRIMARY KEY (`id_pesanan`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -67,7 +124,19 @@ ALTER TABLE `tb_barang`
 -- AUTO_INCREMENT for table `tb_barang`
 --
 ALTER TABLE `tb_barang`
-  MODIFY `tb_brg` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `tb_brg` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `tb_invoice`
+--
+ALTER TABLE `tb_invoice`
+  MODIFY `id_invoice` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `tb_pesanan`
+--
+ALTER TABLE `tb_pesanan`
+  MODIFY `id_pesanan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
