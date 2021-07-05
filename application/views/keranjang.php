f26e96628b2b92fdd0c1aca97f89802bf1c89896
irsyadhani <irsyadhanishubhi@gmail.com>
2021-07-05 09:32:05 +0700

    Add Hapus Keranjang dan Pembayaran

--- application/views/keranjang.php
+++ application/views/keranjang.php
@@ -26,4 +26,11 @@
 				<td align="right">Rp. <?php echo number_format($this->cart->total(), 0,',','.') ?></td>
 			</tr>
 	</table>
+
+	<div align="right">
+		<a href="<?php echo base_url('index.php/dashboard/hapus_keranjang') ?>"><div class="btn btn-sm btn-danger">Hapus Keranjang</div></a>
+		<a href="<?php echo base_url('index.php/dashboard/index') ?>"><div class="btn btn-sm btn-primary">Lanjutkan Keranjang</div></a>
+		<a href="<?php echo base_url('index.php/dashboard/pembayaran') ?>"><div class="btn btn-sm btn-success">Pembayaran</div></a>
+
+	</div>
 </div>
